c32adbbb3b38a5646612f72bdabbce40bcd349cf
Lukas Schwaiger <101287708+LukasSchwaiger@users.noreply.github.com>
2025-02-06 15:53:46 +0100

    fix: pr change requests

--- createJoinedDTM.ps1
+++ createJoinedDTM.ps1
@@ -6,11 +6,30 @@ $outfolderPath = "."
 # Initialize an array to hold the JSON contents
 $jsonArray = @()
 
+$first = $true
+
 # Recursively get all JSON files from the folder and its subfolders
 Get-ChildItem -Path $folderPath -Filter *.json -Recurse | ForEach-Object {
-    $jsonContent = Get-Content -Path $_.FullName -Raw | ConvertFrom-Json
-    $jsonArray += $jsonContent
+    try {
+            $jsonContent = Get-Content -Path $_.FullName -Raw 
+
+            if ($first) {
+                $jsonArray += $jsonContent
+                $first = $false
+            }
+            else {
+                $jsonArray += " , " + $jsonContent
+            }
+      }
+    catch {
+        $errorMessage = $_.Exception.Message
+        Write-Host "Failed to convert file to JSON: $($_.FullName)"
+        Write-Host "Error: $errorMessage"
+        # Optionally, log the error to a file
+        Add-Content -Path .\error_log.txt -Value "Failed to convert file to JSON: $($_.FullName)`nError: $errorMessage`n"
+        throw "Exiting due to error in file: $($_.FullName)"
+    }
 }
 
 # Convert the array to JSON and save it to a file
-$jsonArray | ConvertTo-Json -Depth 100 | Set-Content -Path "$outfolderPath\combinedDTM.json"
+"[" + $jsonArray + "]"  | Set-Content -Path "$outfolderPath\combinedDTM.json"
